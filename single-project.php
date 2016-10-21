a115971f90c2106de02450a8d18a0d4012ac0f01
Alex Rusin <alexrusin_2000@yahoo.com>
2016-10-20 21:59:01 -0700

    Add screen shot

--- single-project.php
+++ single-project.php
@@ -16,11 +16,11 @@ get_header(); ?>
 			get_template_part( 'template-parts/content-project', get_post_format() );
 
 			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popperscores' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next project:', 'popperscores' ) . '</span> ' .
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'westcomfort' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next project:', 'westcomfort' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popperscores' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous project:', 'popperscores' ) . '</span> ' .
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'westcomfort' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous project:', 'westcomfort' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
 			) );
 
